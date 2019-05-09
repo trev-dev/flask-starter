ceab1c3fd0bdde5eb200b1482d3a370588b5f8d7
Trevor Barton <trev@trevdev.ca>
2019-05-08 17:39:38 -0700

    update passenger to use pipenv env

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,8 @@
-import sys, os # noqa
+import sys, os, subprocess # noqa
 
-INTERP = '/home/<username>/<websitefolder>/env/bin/python'
+CMD = ['pipenv', 'run', 'which', 'python3']
+
+INTERP = subprocess.check_output(CMD).strip().decode('utf-8')
 if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
 sys.path.append(os.getcwd())
